09ea1c789a302978f09373b21a6dd7fd935a0236
Julien_P <jparis.py@gmail.com>
2019-08-30 03:47:38 +0200

    fix export problem - fix 11

--- components/UX-components/CardData.vue
+++ components/UX-components/CardData.vue
@@ -463,7 +463,8 @@ export default {
       switch (true) {
         case (textLength + (mobileHandicap - exportAdding) * 1  < 20 ): return 'display-4'
         case (textLength + (mobileHandicap - exportAdding) * 1  < 30 ): return 'display-3'
-        case (textLength + (mobileHandicap - exportAdding) * 1  < 50 ): return 'display-2'
+        case (textLength + (mobileHandicap - exportAdding) * 1  < 40 ): return 'display-2'
+        case (textLength + (mobileHandicap - exportAdding) * 1  < 50 ): return 'display-2-bis'
         case (textLength + (mobileHandicap - exportAdding) * 1  < 70 ): return 'display-1'
         case (textLength + (mobileHandicap - exportAdding) * 1  < 100 ): return 'display-half'
         case (textLength + (mobileHandicap - exportAdding) * 1  < 130 ): return 'headline-custom'
